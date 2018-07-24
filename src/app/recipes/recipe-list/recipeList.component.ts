edfe0932c86a9848874c664ff54b2b4198f7327a
Edson Vladimir Tarqui <edsonvladimir@edsonvladimir.BAGOLIN>
2018-07-24 19:39:34 -0400

    recipe item fixed

--- src/app/recipes/recipe-list/recipeList.component.ts
+++ src/app/recipes/recipe-list/recipeList.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from "../recipe.model";
     styleUrls:['./recipeList.component.css']
 })
 export class RecipeListComponent{
-recipes: Recipe[]=[
+ recipes: Recipe[]=[
     new Recipe('A test Recipe','This is simply a test','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F06%2Fmain%2Frigatoni-kale-pesto-1708p12.jpg%3Fitok%3DZRmUV5oK&w=1000&q=85'),
     new Recipe('A test Recipe','This is simply a test','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F06%2Fmain%2Frigatoni-kale-pesto-1708p12.jpg%3Fitok%3DZRmUV5oK&w=1000&q=85')
 ];
